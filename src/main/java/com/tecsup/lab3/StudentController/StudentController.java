21884dfd29f4f9c039b4477a9b761ee647518cb1
Alumno <Alumno@LAB1507-15>
2023-04-11 15:16:58 -0500

    quinto commit

---
+++ src/main/java/com/tecsup/lab3/StudentController/StudentController.java
@@ -0,0 +1,8 @@
+package com.tecsup.lab3.StudentController;
+
+public class StudentController {
+	public static void main( String[] args )
+    {
+        System.out.println( "Estudiante Controller" );
+    }
+}
