9446edc16a00b4c9202ab487a7ae7808d4141ffa
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2023-04-11 14:41:46 -0500

    Second Commit

---
+++ src/main/java/com/tecsup/lab3/repository/StudentRepository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.tecsup.lab3.repository.StudentRepository;
+
+public class StudentRepository {
+	public static void main( String[] args )
+    {
+        System.out.println( "Repositorio de Estudiantes" );
+    }
+}
